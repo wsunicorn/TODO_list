d4145aba5d7a58b447494176d63c08911e0351da
MinhNguyen1007 <minhnguyen10072004@gmail.com>
2025-03-04 17:37:33 +0700

    release version 1

---
+++ flaskr/admin.py
@@ -0,0 +1,54 @@
+import functools
+from flask import Blueprint, g, redirect, render_template, url_for, flash
+from .auth import login_required
+from .db import get_db
+
+bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+def admin_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        if g.user['role'] != 'admin':
+            flash('Bạn không có quyền truy cập trang này.')
+            return redirect(url_for('index'))
+        return view(**kwargs)
+    return wrapped_view
+
+@bp.route('/')
+@admin_required
+def index():
+    db = get_db()
+    users = db.execute('SELECT * FROM user ORDER BY id').fetchall()
+    return render_template('admin/index.html', users=users)
+
+@bp.route('/delete/<int:id>', methods=['POST'])
+@admin_required
+def delete_user(id):
+    db = get_db()
+    if id == g.user['id']:
+        flash('Bạn không thể xóa chính mình!')
+        return redirect(url_for('admin.index'))
+    
+    db.execute('DELETE FROM todo WHERE user_id = ?', (id,))
+    db.execute('DELETE FROM user WHERE id = ?', (id,))
+    db.commit()
+    flash('User đã được xóa thành công.')
+    return redirect(url_for('admin.index'))
+
+@bp.route('/toggle_role/<int:id>', methods=['POST'])
+@admin_required
+def toggle_role(id):
+    db = get_db()
+    user = db.execute('SELECT * FROM user WHERE id = ?', (id,)).fetchone()
+    
+    if id == g.user['id']:
+        flash('Bạn không thể thay đổi vai trò của chính mình!')
+        return redirect(url_for('admin.index'))
+    
+    new_role = 'admin' if user['role'] == 'user' else 'user'
+    db.execute('UPDATE user SET role = ? WHERE id = ?', (new_role, id))
+    db.commit()
+    flash(f'Vai trò của {user["username"]} đã được thay đổi thành {new_role}.')
+    return redirect(url_for('admin.index'))
---
+++ flaskr/blog.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, g, redirect, render_template, request, url_for
+from .auth import login_required
+from .db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+@login_required
+def index():
+    db = get_db()
+    todos = db.execute(
+        'SELECT * FROM todo WHERE user_id = ? ORDER BY id DESC',
+        (g.user['id'],)
+    ).fetchall()
+    return render_template('blog/index.html', todos=todos)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        db = get_db()
+        db.execute(
+            'INSERT INTO todo (user_id, title) VALUES (?, ?)',
+            (g.user['id'], title)
+        )
+        db.commit()
+        return redirect(url_for('index'))
+    return render_template('blog/create.html')
+
+@bp.route('/update/<int:id>', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    db = get_db()
+    todo = db.execute('SELECT * FROM todo WHERE id = ? AND user_id = ?', (id, g.user['id'])).fetchone()
+
+    if todo is None:
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        completed = 1 if 'completed' in request.form else 0
+        db.execute(
+            'UPDATE todo SET completed = ? WHERE id = ?',
+            (completed, id)
+        )
+        db.commit()
+        return redirect(url_for('index'))
+
+    return render_template('blog/update.html', todo=todo)
+
+@bp.route('/delete/<int:id>', methods=('POST',))
+@login_required
+def delete(id):
+    db = get_db()
+    db.execute('DELETE FROM todo WHERE id = ? AND user_id = ?', (id, g.user['id']))
+    db.commit()
+    return redirect(url_for('index'))
