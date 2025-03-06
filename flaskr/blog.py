01d867b5af59f2e8e465636d2f175056fb18422e
MinhNguyen1007 <minhnguyen10072004@gmail.com>
2025-03-06 12:06:40 +0700

    Cập nhật code cho câu 6  cho nhánh TanMinh

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, g, redirect, render_template, request, url_for, make_response
-
 from .auth import login_required
 from .db import get_db
 
@@ -13,20 +12,24 @@ def index():
         'SELECT * FROM todo WHERE user_id = ? ORDER BY id DESC',
         (g.user['id'],)
     ).fetchall()
-    return render_template('blog/index.html', todos=todos)
+    response = make_response(render_template('blog/index.html', todos=todos))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
+        if not title:
+            return render_template('blog/create.html', error='Tiêu đề không được để trống.')
         db = get_db()
         db.execute(
             'INSERT INTO todo (user_id, title) VALUES (?, ?)',
             (g.user['id'], title)
         )
         db.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('.index'))
     return render_template('blog/create.html')
 
 @bp.route('/update/<int:id>', methods=('GET', 'POST'))
@@ -36,7 +39,7 @@ def update(id):
     todo = db.execute('SELECT * FROM todo WHERE id = ? AND user_id = ?', (id, g.user['id'])).fetchone()
 
     if todo is None:
-        return redirect(url_for('index'))
+        return redirect(url_for('.index'))
 
     if request.method == 'POST':
         completed = 1 if 'completed' in request.form else 0
@@ -45,7 +48,7 @@ def update(id):
             (completed, id)
         )
         db.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('.index'))
 
     return render_template('blog/update.html', todo=todo)
 
@@ -55,4 +58,24 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM todo WHERE id = ? AND user_id = ?', (id, g.user['id']))
     db.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('.index'))
+
+@bp.route('/delete_multiple', methods=['POST'])
+@login_required
+def delete_multiple():
+    db = get_db()
+    data = request.get_json()
+    task_ids = [int(id) for id in data.get('task_ids', [])]
+    if not task_ids:
+        return 'Không có task nào được chọn', 400
+    try:
+        placeholders = ','.join('?' * len(task_ids))
+        db.execute(
+            f'DELETE FROM todo WHERE id IN ({placeholders}) AND user_id = ?',
+            task_ids + [g.user['id']]
+        )
+        db.commit()
+        return '', 200
+    except Exception as e:
+        db.rollback()
+        return f'Lỗi: {str(e)}', 500
